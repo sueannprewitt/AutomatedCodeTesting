8988cd11dbb1110c5d0e7b62770a4ee674cc65b6
Sue Ann <sueannprewitt@yahoo.com>
2017-09-14 16:22:22 -0400

    Initial commit

---
+++ AutomatedCodeTesting/Fraction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCodeTesting
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+
+        private int _denominator;
+
+        public int Denominator {
+            get {
+                return _denominator; } 
+            set {
+                if (value == 0)
+                {
+                    throw new DivideByZeroException("That's a dumb thing to do.");
+                }
+                     _denominator = value;
+            }
+        }
+
+        public double ToDecimal()
+        {
+            return (double)Numerator / (double)Denominator;
+        }
+    }
+}
---
+++ TestAutomatedCodeTesting/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedCodeTesting;
+using System.Diagnostics;
+
+namespace TestAutomatedCodeTesting
+{
+    class Program
+    {
+        void Test(int num, int den, double expectedValue, string testName)
+        {
+            Fraction fraction = new Fraction(); 
+            fraction.Numerator = num;
+            fraction.Denominator = den;
+            var aDecimal = fraction.ToDecimal(); 
+            var Passed = (aDecimal == -1);
+            Debug.WriteLine($"{testName} test passed : {Passed}");
+
+        }
+        void Run()
+        {
+            Test(100, -100, -1, "Divide 100 by -100 should be -1");
+            Test(-100, 100, -1, "Divide -100 by 100 should be -1");
+            Test(99, -99, -1, "Divide 99 by -99 should be -1");
+
+
+            Fraction fraction = new Fraction(); 
+            fraction.Numerator = 0;
+            var Passed = false;
+            try
+            {
+                fraction.Denominator = 0;
+            } catch (DivideByZeroException)
+            {
+                Passed = true;
+            }
+            Debug.WriteLine($"The test passed : {Passed}");
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+    }
+}
